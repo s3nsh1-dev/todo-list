ebe67ccf20022d95dd876b5eee4f165599d31db1
s3nsh1-dev <shubham228pandey@gmail.com>
2025-03-27 04:04:27 +0530

    SOD 27th March 2025

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,5 +1,34 @@
+import { Box, Typography, Button } from "@mui/material";
+
 const Homepage = () => {
-  return <div>this is a todo list</div>;
+  return (
+    <>
+      <Box component={"header"}>
+        <Typography variant="h3">what ToDo...?</Typography>
+      </Box>
+      <Box component="main" className="">
+        <Box>
+          <Button variant="contained" color="primary">
+            Daily Tasks
+          </Button>
+          <Button variant="contained" color="secondary">
+            Weekly Goals
+          </Button>
+          <Button variant="contained" color="warning">
+            Monthly Goals
+          </Button>
+          <Button variant="contained" color="error">
+            Year Resolution
+          </Button>
+        </Box>
+        <Box>
+          <Button variant="contained" color="success">
+            NOTES
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Homepage;
