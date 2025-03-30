7cb7238513ff844d3b0fd724e8e19c51908db1a0
s3nsh1-dev <shubham228pandey@gmail.com>
2025-03-30 14:23:21 +0530

    the delegation is done

--- client/src/components/DailyTasks/ShowOngoingTasks.tsx
+++ client/src/components/DailyTasks/ShowOngoingTasks.tsx
@@ -6,7 +6,9 @@ import type { taskDetailsType } from "../../constants/commonInterfaces";
 import { useDispatch } from "react-redux";
 import { updateTask } from "../../redux/slices/dailyTasksSlice";
 import { addPanelStyle } from "../../constants/customCssProperties";
-import { Button, IconButton, Modal } from "@mui/material";
+import { IconButton, Modal } from "@mui/material";
+import TaskCardActionButtons from "./add-tasks/TaskCardActionButtons";
+import TaskCardLabelContent from "./add-tasks/TaskCardLabelContent";
 
 interface propTypes {
   index: number;
@@ -50,39 +52,20 @@ const ShowOngoingTasks: FC<propTypes> = ({ tasks, index, arrLength }) => {
       {open && (
         <Modal open={open} onClose={onClose}>
           <section className="bg-gray-800" style={addPanelStyle}>
-            <article id="add-dailyTaskName" className="text-gray-300">
-              <h1 className="text-center text-2xl font-bold ">Add New Task</h1>
-              <DividerGray />
-              <div style={{ marginTop: "20px" }}>
-                <p className="font-bold">Task Name</p>
-                <input
-                  className="border border-gray-300 rounded-md p-2 w-full"
-                  style={{ padding: "10px", margin: "5px 0px" }}
-                  value={userValue}
-                  type="text"
-                  id="add-dailyTaskName"
-                  placeholder={tasks.taskName}
-                  onChange={(e) => setUserValue(e.target.value)}
-                />
-              </div>
-            </article>
+            <TaskCardLabelContent
+              userValue={userValue}
+              setUserValue={setUserValue}
+              heading="Edit Task"
+              label="Task Name"
+            />
             <article>
-              <div
-                id="add-dailyTask-actionButtons"
-                className="flex justify-center item-center gap-3"
-              >
-                <Button color="error" variant="contained" onClick={onClose}>
-                  Cancel
-                </Button>
-                <Button
-                  onClick={submitEditedTask}
-                  variant="contained"
-                  color="success"
-                  disabled={isDisabled}
-                >
-                  SAVE
-                </Button>
-              </div>
+              <TaskCardActionButtons
+                isDisabled={isDisabled}
+                handleSubmit={submitEditedTask}
+                handleClose={onClose}
+                backLabel="Cancel"
+                enterLabel="Save"
+              />
             </article>
           </section>
         </Modal>
