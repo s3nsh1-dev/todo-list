44b2581453d844676ad8c5749733b77168a3b3ab
s3nsh1-dev <shubham228pandey@gmail.com>
2025-04-02 18:50:14 +0530

    SOD 2apr

--- client/src/components/weekly-goals/WeeklyBody.tsx
+++ client/src/components/weekly-goals/WeeklyBody.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { RootState } from "../../redux/store";
 import IntroToManagement from "../common/IntroToManagement";
 import CompletedContainer from "../common/CompletedContainer";
@@ -11,11 +11,19 @@ import {
   updateWeeklyGoalStatus,
   updateWeeklyGoalName,
 } from "../../redux/slices/weeklyGoalsSlice";
+import { Modal, Button } from "@mui/material";
 
 const content =
   "The `weeklySlice` manages weekly goals, allowing users to add, remove, and update tasks efficiently. It leverages Immer for immutability, enabling direct state modifications. Goals can be added with a unique ID, removed by filtering, or updated by modifying their name—all while keeping Redux state management seamless and optimized.";
 
 const WeeklyBody = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [userValue, setUserValue] = useState<string>("");
+  const [editData, setEditData] = useState<{ id: string; oldName: string }>({
+    id: "",
+    oldName: "",
+  });
+
   const GG = useSelector(
     (state: RootState) => state.weeklyGoals.weeklyGoalsList
   );
@@ -35,12 +43,25 @@ const WeeklyBody = () => {
     dispatch(updateWeeklyGoalStatus(value));
   };
 
+  const handleOnChangeInput = (value: string) => {
+    setUserValue(value);
+  };
+
   const handleDeleteGoal = (value: string) => {
     dispatch(removeWeeklyGoals(value));
   };
 
   const handleEditWGoal = ({ id, name }: { id: string; name: string }) => {
-    dispatch(updateWeeklyGoalName({ id, name }));
+    setEditData({ id, oldName: name });
+  };
+
+  const toggleModal = () => {
+    setOpen((open) => !open);
+  };
+
+  const handleSubmit = () => {
+    dispatch(updateWeeklyGoalName({ id: editData.id, name: userValue }));
+    toggleModal();
   };
 
   const renderCompletedWTasks = completedWGoals.map((goal, index) => {
@@ -79,6 +100,29 @@ const WeeklyBody = () => {
       <OngoingContainer heading="Ongoing Goals">
         {renderOngoingWGoals}
       </OngoingContainer>
+      {open && (
+        <Modal open={open} onClose={toggleModal}>
+          <div className="bg-gray-800">
+            <div>
+              <p>Goal Name</p>
+              <input
+                type="text"
+                value={userValue}
+                placeholder={editData.oldName}
+                onChange={(event) => {
+                  handleOnChangeInput(event.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <Button color="error" onClick={toggleModal}>
+                Close
+              </Button>
+              <Button color="success" onClick={handleSubmit}></Button>
+            </div>
+          </div>
+        </Modal>
+      )}
     </>
   );
 };
