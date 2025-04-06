44e707a2e485715d9cad1e2f9b533ff8f784fa66
s3nsh1-dev <shubham228pandey@gmail.com>
2025-04-06 23:54:07 +0530

    re-learing context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
+import { GlobalProvider } from "./context/GlobalContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GlobalProvider>
   </Provider>
 );
