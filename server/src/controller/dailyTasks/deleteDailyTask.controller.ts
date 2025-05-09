4c55431fe74c0f6b822211dad52328c10c0bb639
s3nsh1-dev <shubham228pandey@gmail.com>
2025-05-09 15:32:19 +0530

    Delete added: CRUD complete

---
+++ server/src/controller/dailyTasks/deleteDailyTask.controller.ts
@@ -0,0 +1,22 @@
+import { RequestHandler, Request, Response } from "express";
+import DailyTask from "../../model/dailyTask.model";
+
+const deleteTask: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    const { taskId }: { taskId: string } = { ...req.body };
+    if (!taskId) {
+      console.log(taskId);
+      res.status(400).json({ message: "ID not found" });
+      return;
+    }
+    const result = await DailyTask.deleteOne({ taskId });
+    if (result.deletedCount === 0) {
+      res.status(404).json({ message: "Task not found" });
+      return;
+    }
+    res.status(200).json({ message: "SUCCESS: TASK DELETED", body: result });
+  } catch (error) {
+    res.status(500).json({ message: `Server Error: ${error}` });
+  }
+};
+export default deleteTask;
--- server/src/routes/daily.routes.ts
+++ server/src/routes/daily.routes.ts
@@ -3,11 +3,13 @@ import fetchDailyTasks from "../controller/dailyTasks/fetchDailyTasks.controller
 import addDailyTask from "../controller/dailyTasks/addDailyTask.controller";
 import updateDailyTaskName from "../controller/dailyTasks/updateDailyName.controller";
 import updateDailyTaskStatus from "../controller/dailyTasks/updateDailyStatus.controller";
+import deleteTask from "../controller/dailyTasks/deleteDailyTask.controller";
 
 const taskRoutes = express.Router();
 
 taskRoutes.get("/", fetchDailyTasks);
 taskRoutes.post("/add", addDailyTask);
+taskRoutes.delete("/delete", deleteTask);
 taskRoutes.post("/update/name", updateDailyTaskName);
 taskRoutes.patch("/update/status", updateDailyTaskStatus);
 
