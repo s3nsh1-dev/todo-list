38e3ea30f4ba44f1f2b56c9b9c2820b585d682dc
s3nsh1-dev <shubham228pandey@gmail.com>
2025-05-15 23:22:42 +0530

    add Task modified for order prop

--- server/src/controller/monthlyTasks/addMonthlyTasks.controller.ts
+++ server/src/controller/monthlyTasks/addMonthlyTasks.controller.ts
@@ -9,8 +9,15 @@ const addMonthlyTask: RequestHandler = async (req: Request, res: Response) => {
       res.status(422).json({ message: "Some Properties Missing" });
       return;
     }
-    const monthly: monthlyGoalsListType = await MonthlyTask.create(result);
-    res.status(201).json({ message: "ADDED: MONTHLY TASKS", body: monthly });
+
+    //find the highest order number
+    const lastTask = await MonthlyTask.findOne({}).sort({ order: -1 }).limit(1);
+    const newOrder = lastTask ? lastTask.order + 1 : 0;
+    const task = await MonthlyTask.create({
+      GoalName: result.GoalName,
+      order: newOrder,
+    });
+    res.status(201).json({ message: "ADDED: MONTHLY TASKS", body: task });
   } catch (error) {
     res.status(500).json({ message: `Server Error: ${error}` });
   }
